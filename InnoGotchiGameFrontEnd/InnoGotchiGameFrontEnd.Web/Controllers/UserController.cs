3670c84b174c5c9860ef569e70df7cc7ac18a64a
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-04 16:54:26 +0300

    Create UserController

---
+++ InnoGotchiGameFrontEnd/InnoGotchiGameFrontEnd.Web/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using InnoGotchiGameFrontEnd.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnoGotchiGameFrontEnd.Web.Controllers
+{
+	public class UserController : Controller
+	{
+		private UserService _userService;
+
+
+		public UserController(UserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var users = await _userService.OnGet();
+			return View(users);
+		}
+
+		[HttpGet]
+		[Route("users/{userId}")]
+        public async Task<IActionResult> UserPage(int userId)
+        {
+            var user = await _userService.OnGet(userId);
+            return View(user);
+        }
+        [HttpGet]
+		public IActionResult LogIn()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(string email, string password)
+        {
+			await _userService.Authorize(email, password);
+			return Redirect("/");
+        }
+    }
+}
