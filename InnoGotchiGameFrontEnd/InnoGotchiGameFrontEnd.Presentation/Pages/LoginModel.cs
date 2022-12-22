90f2f12aefcec6527465ce20b35aee87d55cf9c6
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-22 13:21:13 +0300

    Create Login page

---
+++ InnoGotchiGameFrontEnd/InnoGotchiGameFrontEnd.Presentation/Pages/LoginModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace InnoGotchiGameFrontEnd.Presentation.Pages
+{
+    public class LoginModel : ComponentBase
+    {
+        public LoginModel()
+        {
+            LoginData = new LoginViewModel();
+        }
+
+        public LoginViewModel LoginData { get; set; }
+
+        protected Task LoginAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(14, ErrorMessage = "too long")]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
