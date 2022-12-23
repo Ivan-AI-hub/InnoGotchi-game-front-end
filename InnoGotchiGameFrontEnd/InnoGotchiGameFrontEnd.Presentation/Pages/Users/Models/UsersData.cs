ace5c9b0e570b787e9d83dfe5e0d8787c2ab9e6a
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-23 16:01:31 +0300

    Create UsersData.cs

---
+++ InnoGotchiGameFrontEnd/InnoGotchiGameFrontEnd.Presentation/Pages/Users/Models/UsersData.cs
@@ -0,0 +1,21 @@
+﻿using InnoGotchiGameFrontEnd.BLL.Model;
+using InnoGotchiGameFrontEnd.DAL.Models.Users;
+using InnoGotchiGameFrontEnd.Presentation.Components.PageSystem;
+
+namespace InnoGotchiGameFrontEnd.Presentation.Pages.Users.Models
+{
+    public class UsersData
+    {
+        public IEnumerable<UserDTO> Users { get; set; }
+        public UserFiltrator Filtrator { get; set; }
+        public UserSorter Sorter { get; set; }
+        public Page Page { get; set; }
+        public UsersData()
+        {
+            Users = new List<UserDTO>();
+            Filtrator = new UserFiltrator();
+            Sorter = new UserSorter();
+            Page = new Page(1, 10);
+        }
+    }
+}
