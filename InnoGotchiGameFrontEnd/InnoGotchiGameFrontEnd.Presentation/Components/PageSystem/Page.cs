fa8c060ca341642ed33754f40637bfde12576ae9
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-23 22:34:00 +0300

    Create classes for pagination

---
+++ InnoGotchiGameFrontEnd/InnoGotchiGameFrontEnd.Presentation/Components/PageSystem/Page.cs
@@ -0,0 +1,32 @@
+﻿namespace InnoGotchiGameFrontEnd.Presentation.Components.PageSystem
+{
+    public class Page
+    {
+        public Page(int number, int pageSize)
+        {
+            Number = number;
+            PageSize = pageSize;
+        }
+
+        public int Number { get; set; }
+        public int PageSize { get; set; }
+
+        public PageStatus GetPageStatus(int itemsCount)
+        {
+            PageStatus pageStatus;
+            if (Number <= 1)
+            {
+                pageStatus = PageStatus.FirstPage;
+            }
+            else if (itemsCount < PageSize)
+            {
+                pageStatus = PageStatus.LastPage;
+            }
+            else
+            {
+                pageStatus = PageStatus.MiddlePage;
+            }
+            return pageStatus;
+        }
+    }
+}
---
+++ InnoGotchiGameFrontEnd/InnoGotchiGameFrontEnd.Presentation/Components/PageSystem/PageStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace InnoGotchiGameFrontEnd.Presentation.Components.PageSystem
+{
+    public enum PageStatus
+    {
+        FirstPage,
+        MiddlePage,
+        LastPage
+    }
+}
