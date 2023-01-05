3b9c8be7e73b790bf60780a2ed40976d7112f910
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-05 23:29:57 +0300

    Add pets page

---
+++ InnoGotchiGameFrontEnd/InnoGotchiGameFrontEnd.Presentation/Pages/Pets/Models/PetsData.cs
@@ -0,0 +1,26 @@
+﻿using InnoGotchiGameFrontEnd.BLL.Filtrators;
+using InnoGotchiGameFrontEnd.BLL.Model.Pet;
+using InnoGotchiGameFrontEnd.BLL.Sorters;
+using InnoGotchiGameFrontEnd.Presentation.Components.PageSystem;
+
+namespace InnoGotchiGameFrontEnd.Presentation.Pages.Pets.Models
+{
+    public class PetsData
+    {
+        public IEnumerable<PetDTO>? Pets { get; set; }
+        public PetDTOFiltrator Filtrator { get; set; }
+        public PetDTOSorter Sorter { get; set; }
+        public Page Page { get; set; }
+        public PageStatus PageStatus => Page.GetPageStatus(Pets == null? 0 : Pets.Count());
+        public PetsData()
+        {
+            Pets = null;
+            Filtrator = new PetDTOFiltrator();
+            Sorter = new PetDTOSorter() 
+            { 
+                SortRule = PetDTOSortRule.happinessDays
+            };
+            Page = new Page(1, 15);
+        }
+    }
+}
